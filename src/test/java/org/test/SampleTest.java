543e2fc0a99bf9aa63d2f30bcaf9314dd5d80c1a
SekarMano14 <sekarmano46@gmail.com>
2024-09-11 09:08:18 +0530

    Mano's Code

--- src/test/java/org/test/SampleTest.java
+++ src/test/java/org/test/SampleTest.java
@@ -4,6 +4,11 @@ public class SampleTest {
 
 	public static void main(String[] args) {
 		System.out.println("Test");
-		//here start to write implementations
+		System.out.println("Test1");
+		System.out.println("Test2");
+		System.out.println("Test3");
+		
+
+
 	}
 }
